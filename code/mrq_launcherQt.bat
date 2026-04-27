4e72f057bf357cd6466519fdbdee5b9658f9e695
muhamaxis <muhamax@gmail.com>
2026-04-27 19:25:32 -0400

    qt but

---
+++ code/mrq_launcherQt.bat
@@ -0,0 +1,10 @@
+@echo off
+REM MRQ Launcher starter
+REM Specify Python path here if it is not set in PATH
+set PYTHON_EXE=python
+
+REM Path to the script
+set SCRIPT_PATH=%~dp0mrq_launcher.py
+
+%PYTHON_EXE% "%SCRIPT_PATH%" --qt
+pause
